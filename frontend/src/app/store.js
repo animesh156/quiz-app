4f6a2bc77ad4ea43f01f0e684ebff24b6383bad1
Animesh  Rathore <animeshrathore255@gmail.com>
2024-09-09 03:15:36 +0530

    Update store.js

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,6 +1,6 @@
 import {configureStore} from '@reduxjs/toolkit'
 import authReducer from '../features/auth/authSlice'
-import quizOptionsReducer from '../features/quiz/quizSlice'
+import quizOptionsReducer from '../features/quizSlice'
 
 export const store = configureStore(
     {
