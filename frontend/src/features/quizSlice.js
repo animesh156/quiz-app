adc81dfda79ac4da5d299e87224211a52c8cf1c0
Animesh  Rathore <animeshrathore255@gmail.com>
2024-09-09 03:13:59 +0530

    Add files via upload

---
+++ frontend/src/features/quizSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    category: '17', // Default value for Science
+  difficulty: 'easy', // Default difficulty
+  totalQuestion: '5', // Default total questions
+  type: 'multiple', // Default type (Multiple Choice)
+}
+
+const quizSlice = createSlice(
+    {
+        name: 'quizOptions',
+        initialState,
+        reducers: {
+            setCategory: (state, action) => {
+                state.category = action.payload
+            },
+            setDifficulty: (state,action) => {
+                state.difficulty = action.payload
+            },
+            setTotalQuestions: (state,action) => {
+            state.totalQuestion = action.payload
+            },
+            setType: (state,action) => {
+                state.type = action.payload
+            },
+        },
+    }
+);
+
+export const {setCategory, setDifficulty, setTotalQuestions, setType} = quizSlice.actions;
+export default quizSlice.reducer;
