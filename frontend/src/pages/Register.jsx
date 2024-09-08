ecd5048a00310f09887412e6e4e783668bf95750
Animesh  Rathore <animeshrathore255@gmail.com>
2024-09-09 00:41:03 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -11,7 +11,7 @@ function Register() {
     name: '',
     email: '',
     password: '',
-    password2: '',
+  
   })
 
   const { name, email, password } = formData
@@ -106,7 +106,7 @@ function Register() {
           </div>
          
           <div className='form-group'>
-          <button type="button" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Sign Up</button>
+          <button type="submit" className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Sign Up</button>
           </div>
 
           <div>
