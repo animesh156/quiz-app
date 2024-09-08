9fe66959eda8cd8c88797521f4e7878d2c545335
Animesh  Rathore <animeshrathore255@gmail.com>
2024-09-09 03:06:13 +0530

    Update Quiz.jsx

--- frontend/src/pages/Quiz.jsx
+++ frontend/src/pages/Quiz.jsx
@@ -1,191 +1,156 @@
 /* eslint-disable react/prop-types */
-import  { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-import Result from '../components/Result'
-import Spinner from '../components/Spinner';
-import axios from 'axios';
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import {setCategory, setDifficulty, setTotalQuestions, setType} from '../features/quiz/quizSlice'
+
+import Spinner from '../components/Spinner'
 
 
+import { reset } from '../features/auth/authSlice'
 
-const Quiz = () => {
-  const [questions, setQuestions] = useState([]);
-  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [score, setScore] = useState(0);
-  const [isQuizFinished, setIsQuizFinished] = useState(false);
-  const [timer, setTimer] = useState(15);
-
-
-   const category = useSelector((state) => state.quizOptions.category);
-  const difficulty = useSelector((state) => state.quizOptions.difficulty);
-  const totalQuestion = useSelector((state) => state.quizOptions.totalQuestion);
-  const type = useSelector((state) => state.quizOptions.type);
-
-
-  // Shuffle function for the answers
-  const shuffleArray = (array) => {
-    return array.sort(() => Math.random() - 0.5);
-  };
-
-  const handleQuizCompletion = () => {
-    setIsQuizFinished(true);
-  setUserScore(score);
-  };
-
-  // Fetch quiz data when the component mounts
-  useEffect(() => {
-    const getQuizData = async () => {
-      try {
-        const response =  await axios.get(`http://localhost:4000/quiz?amount=${totalQuestion}&category=${category}&difficulty=${difficulty}&type=${type}`, {
-          headers: {
-            'Authorization': `Bearer ${token}`
-          }
-        })
-        if(!response.data) throw new Error('client error')
-          
-        const quizQuestions = response.data.results
-       
-
-        if (quizQuestions && Array.isArray(quizQuestions)) {
-          const formattedQuestions = quizQuestions.map((question) => {
-            const answers = shuffleArray([
-              ...question.incorrect_answers,
-              question.correct_answer,
-            ]);
-            return { ...question, answers };
-          });
-
-          setQuestions(formattedQuestions);
-        } else {
-          console.error("Quiz data is not in the expected format.");
-        }
-
-      } catch (err) {
-        console.error("Error fetching quiz data:", err);
-      }
-    };
-
-    getQuizData();
-  }, []);  
-
-
-  useEffect(() => {
-    if (timer === 0) {
-      
-      handleNextQuestion();
-      return;
-    }
-
-    const countdown = setInterval(() => {
-      setTimer((prevTimer) => prevTimer - 1);
-    }, 1000);
-
-    // Cleanup the interval when the component unmounts or when the timer is reset
-    return () => clearInterval(countdown);
-  }, [timer]);
-
-  useEffect(() => {
-    setTimer(15);
-  }, [currentQuestionIndex,questions]);
-
-
-
-
-
-  // Handle the user's answer selection
-  const handleAnswer = (isCorrect,index) => {
-   
-    if (isCorrect) setScore(score + 1);
-  
-    handleNextQuestion(index)
-
-  }
-
-    // Move to the next question or finish the quiz
-    const handleNextQuestion = () => {
-
-   
-    const nextQuestionIndex = currentQuestionIndex + 1;
-    if (nextQuestionIndex < questions.length) {
-              
-      setCurrentQuestionIndex(nextQuestionIndex);
-    } else {
-      handleQuizCompletion()
-    }
-  }
-
-    const getToken = () => {
-    const user = localStorage.getItem('user');  // Retrieve the user object from localStorage
-    if (user) {
-      const parsedUser = JSON.parse(user);      // Parse the JSON string back into an object
-      return parsedUser.token;                  // Access the token from the parsed object
-    }
-    return null;  // Return null if there's no user data
-  };
-  
-  
-  
-  const token = getToken()
-
-  const user = localStorage.getItem('user')
-  const userid = JSON.parse(user)._id
-  const userName = JSON.parse(user).name
-
-const setUserScore = async(score) => {
-  try {
-    const res = await axios.post('http://localhost:4000/score', {score,userid,userName}, {headers: {
-     'Authorization': `Bearer ${token}`
-   }})
-      if(res.data) return true
-   } catch (error) {
-     console.log(`Error sending user score`,error)
-     return false
-   }
- }
-
-  
+function Dashboard() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
  
-  
 
-  if (!questions.length) {
+
+  const { user, isLoading, isError, message } = useSelector((state) => state.auth)
+ 
+
+  useEffect(() => {
+    
+    if (!user) {
+      navigate('/login')
+    }
+   
+   
+    if (isError) {
+      console.log(message)
+    }
+   
+
+   
+
+    return () => {
+      dispatch(reset())
+    }
+  }, [user, navigate,isError,message,  dispatch])
+
+  if (isLoading) {
     return <Spinner />
   }
 
-
-
-  if (isQuizFinished) {
+  const onSubmit = (e) => {
+    e.preventDefault()
+      
+      
+    
+    navigate('/quiz')
    
-    return <Result score={score} totalQuestions={questions.length} />
   }
 
-  const currentQuestion = questions[currentQuestionIndex];
-
   return (
-    <div className='border-cyan-50 shadow-md shadow-green-500 border-2 bg-gradient-to-r from-cyan-300 to-pink-400 w-auto m-auto rounded-lg'>
-      <h2 className='text-2xl font-bold mt-3'>Question {currentQuestionIndex + 1} of {questions.length}</h2>
-      <div className='border-black border-2 mt-8 bg-gray-50 w-auto px-3 py-2 rounded-2xl max-w-md m-auto overflow-auto'>
-      <h3 className='dark:text-red-500 font-bold'>{currentQuestion.question}</h3>
-      </div>
-      
-      <div className='grid grid-cols-1 md:grid-cols-2 mt-2 gap-y-5 gap-x-4 content-center max-h-max md:h-60 ' >
-        {currentQuestion.answers.map((answer, index) => (
-        
-          <button
-          key={index}
-          className='border-cyan-100 hover:shadow-md hover:shadow-orange-400 bg-slate-50  font-semibold dark:text-pink-600 border-2 w-64 max-w-md  overflow-auto px-2 py-2 mt-4 rounded-2xl m-auto'
-            
-            onClick={() => handleAnswer(answer === currentQuestion.correct_answer,index)}
-           
-          >
-            {answer}
-          </button>
-          
-        ))}
-       
-      </div>
-      <div className=' mb-2 mt-5'>{timer < 4 ? <h3 className='text-5xl font-bold text-red-600 '>{timer}</h3> : <h3 className='text-5xl font-bold dark:text-white '>{timer}</h3>}</div>
-    </div>
-  );
-};
+    
+    <>
+    
+    
+   
 
-export default Quiz;
+   
+    <section className='heading'>
+      <h1 className='text-3xl dark:text-rose-700'>Welcome {user && user.name}</h1>
+    </section>
+
+    <form className="max-w-md mx-auto border-cyan-300 border-2 px-5 py-3 rounded-2xl shadow-md shadow-pink-400" onSubmit={onSubmit}>
+<label
+  htmlFor="small"
+  className="block mb-2 text-sm  text-gray-900 dark:text-orange-400 font-semibold font-mono"
+>
+  Category
+</label>
+<select
+  id="small" onChange={(e) => dispatch(setCategory(e.target.value))}
+  className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+>
+ 
+<option selected value="17">Science</option>
+  <option  value="23">History</option>
+ 
+  <option value="21">Sports</option>
+  <option value="27">Animal</option>
+  <option value="20">Mythology</option>
+ 
+</select>
+
+
+<label
+  htmlFor="small"
+  className="block mb-2 text-sm font-mono text-gray-900 dark:text-orange-400 font-semibold"
+>
+  Difficulty
+</label>
+<select
+  id="small"
+  onChange={(e) => dispatch(setDifficulty(e.target.value))}
+  className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+>
+ 
+  <option selected="" value="easy">Easy</option>
+  <option value="medium">Medium</option>
+  <option value="hard">Hard</option>
+ 
+</select>
+
+<label
+  htmlFor="small"
+  className="block mb-2 text-sm font-semibold font-mono  text-gray-900 dark:text-orange-400"
+>
+  Total Question
+</label>
+<select
+  id="small"
+  onChange={(e) => dispatch(setTotalQuestions(e.target.value))}
+  className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+>
+
+  <option selected="" value="5">5</option>
+  <option value="10">10</option>
+  <option value="15">15</option>
+  <option value="20">20</option>
+</select>
+
+<label
+  htmlFor="small"
+  className="block mb-2 text-sm font-semibold font-mono text-gray-900 dark:text-orange-400"
+>
+  Type
+</label>
+<select
+  id="small"
+  onChange={(e) => dispatch(setType(e.target.value))}
+  className="block w-full p-2 mb-6 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+>
+
+  <option selected="" value="multiple">Multiple Choice</option>
+  <option value="boolean">True or False</option>
+  
+</select>
+
+
+
+<button type="submit" className="text-black font-mono bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-lg dark:shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Quiz</button>
+
+</form> 
+
+
+
+</>
+    
+  
+
+  )
+}
+
+export default Dashboard
