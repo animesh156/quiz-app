3515ed76c9813d2cb83083640c2f9ece347668f7
Animesh Singh <animeshrathore255@gmail.com>
2025-12-06 19:06:09 +0530

    feat: add health api route for server & db checkup

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const port = process.env.PORT;
 const cookieParser = require("cookie-parser");
 const { protect } = require("./middleware/authMiddleware");
 var cors = require("cors");
+const mongoose = require("mongoose");
 
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
@@ -22,6 +23,39 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
+
+function formatUptime(seconds) {
+  const hrs = Math.floor(seconds / 3600);
+  const mins = Math.floor((seconds % 3600) / 60);
+  const secs = Math.floor(seconds % 60);
+
+  return `${hrs}h ${mins}m ${secs}s`;
+}
+
+function getISTTimestamp() {
+  return new Date().toLocaleString("en-IN", {
+    timeZone: "Asia/Kolkata",
+  });
+}
+
+//  Health check endpoint
+app.get("/health", (req, res) => {
+  const dbState = mongoose.connection.readyState;
+  const status = dbState === 1 ? "healthy" : "unhealthy";
+
+  const uptimeInSeconds = process.uptime();
+
+  res.status(status === "healthy" ? 200 : 500).json({
+    status,
+    timestampIST: getISTTimestamp(),
+    uptime: formatUptime(uptimeInSeconds),
+    database: {
+      state: dbState,
+      description: dbState === 1 ? "Connected" : "Not Connected ❌",
+    },
+  });
+});
+
 app.use("/api/quiz", quizRoute);
 app.use("/api/user", userRoutes);
 app.use("/api/score", scoreRoute);
